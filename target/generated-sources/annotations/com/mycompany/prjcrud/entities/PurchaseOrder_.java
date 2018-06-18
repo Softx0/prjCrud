aee4303359cf390f803fd20fbf6f4bc4adc367a9
Softx0 <ing.valen0421@hotmail.com>
2018-06-18 11:22:42 -0900

    project build

--- target/generated-sources/annotations/com/mycompany/prjcrud/entities/PurchaseOrder_.java
+++ target/generated-sources/annotations/com/mycompany/prjcrud/entities/PurchaseOrder_.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-17T23:43:48")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-06-18T11:19:08")
 @StaticMetamodel(PurchaseOrder.class)
 public class PurchaseOrder_ { 
 
